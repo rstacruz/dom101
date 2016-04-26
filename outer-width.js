5402a02b74cc68cc4788f67f6ea35d7050c84bbb
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-04-26 15:36:11 +0800

    outer-width: fix marginLeft computation (#57)

--- outer-width.js
+++ outer-width.js
@@ -9,7 +9,7 @@
 function outerWidth (el, includeMargin) {
   var style = typeof window.getComputedStyle !== 'undefined' && window.getComputedStyle(el) || el.currentStyle;
 
-  return el.offsetWidth + (includeMargin && (parseInt(style['marginleft'], 10) + parseInt(style['marginRight'], 10)) || 0);
+  return el.offsetWidth + (includeMargin && (parseInt(style['marginLeft'], 10) + parseInt(style['marginRight'], 10)) || 0);
 }
 
 module.exports = outerWidth;
